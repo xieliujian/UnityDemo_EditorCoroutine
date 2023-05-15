9140875d890745da22ed1c579403547111dbd8e5
xieliujian <377624289@qq.com>
2023-05-15 13:44:25 +0800

    1

---
+++ Assets/Scripts/Editor/EditorCoroutineTest.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorCoroutineTest
+{
+    [MenuItem("EditorCoroutine/Test")]
+    static void Test()
+    {
+        EditorCoroutine.StartEditorCoroutine(CountToTenCoroutine());
+    }
+
+    private static IEnumerator CountToTenCoroutine()
+    {
+        for (var i = 1; i <= 10; ++i)
+        {
+            Debug.LogFormat("{0}", i);
+
+            // yield until next EditorApplication.update
+            yield return null;
+
+            // yield until 1 second has passed
+            yield return new WaitForSeconds(1);
+        }
+
+        // yield until a subroutine has completed
+        yield return CountToTenCompletedSubroutine();
+    }
+
+    private static IEnumerator CountToTenCompletedSubroutine()
+    {
+        Debug.LogFormat("DONE!");
+
+        yield break;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # 编辑器协程
 
+## 示例
+
+```cs
+
+[MenuItem("EditorCoroutine/Test")]
+static void Test()
+{
+    EditorCoroutine.StartEditorCoroutine(CountToTenCoroutine());
+}
+
+private static IEnumerator CountToTenCoroutine()
+{
+    for (var i = 1; i <= 10; ++i)
+    {
+        Debug.LogFormat("{0}", i);
+
+        // yield until next EditorApplication.update
+        yield return null;
+
+        // yield until 1 second has passed
+        yield return new WaitForSeconds(1);
+    }
+
+    // yield until a subroutine has completed
+    yield return CountToTenCompletedSubroutine();
+}
+
+private static IEnumerator CountToTenCompletedSubroutine()
+{
+    Debug.LogFormat("DONE!");
+
+    yield break;
+}
+
+```
